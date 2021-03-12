4f71a7836c2a8a2d88fe01913b090253b886d750
Lmibrahim <lameesibrahim89@gmail.com>
2021-03-12 23:03:28 +0400

    new backend

---
+++ routes.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//require the Express module
+const express = require("express");
+
+//creates a new router object
+const routes = express.Router();
+
+//exports routes
+module.exports = routes;
