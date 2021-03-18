e0010fd13f75fef645139390f504e00f75d6760d
Lmibrahim <lameesibrahim89@gmail.com>
2021-03-18 23:01:36 +0400

    trying to update display

--- shows.api.js
+++ shows.api.js
@@ -24,7 +24,7 @@ shows.get("/diyshows/:id", (req, res) => {
 
 shows.post("/diyshows", (req, res) => {
   let query =
-    "INSERT INTO diyshows (title, start, description, address, diy, lat, lng) VALUES ($1::varchar, $2::timestamp, $3::text, $4::text, $5::boolean, $6::real, $7::real)";
+    "INSERT INTO diyshows (title, start, description, address, diy, lat, lng, display) VALUES ($1::varchar, $2::timestamp, $3::text, $4::text, $5::boolean, $6::real, $7::real, $8::boolean)";
   pool
     .query(query, [
       req.body.title,
@@ -34,11 +34,24 @@ shows.post("/diyshows", (req, res) => {
       true,
       req.body.lat,
       req.body.lng,
+      false
     ])
     .then((response) => {
       res.json(req.body);
     });
 });
+
+shows.put("/diyshows/:id", (req, res)=> {
+  console.log('got here');
+  let showId = req.params.id;
+  let query =
+  `UPDATE diyshows SET display=true WHERE diyshows.id = ${showId}`;
+  pool.query(query)
+  .then((response)=> {
+    res.json(req.body)
+  })
+}
+);
 
 // export routes for use in server.js
 module.exports = shows;
